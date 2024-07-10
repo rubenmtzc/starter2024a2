2c86ba346853991b51890408f55abb8370b00cea
rmc509 <rubenmtzc@gmail.com>
2024-07-10 15:38:01 +0100

    Copy from tturocy after lunch

---
+++ encodtask/encodtask.py
@@ -0,0 +1,15 @@
+import random
+
+from otree.api import (
+    BaseConstants,
+    BaseGroup,
+    BasePlayer,
+    BaseSubsession,
+    Page,
+    WaitPage,
+    models,
+)
+
+doc = """
+An encoding task made by me
+"""
